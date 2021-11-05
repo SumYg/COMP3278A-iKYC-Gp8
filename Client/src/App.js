d9f5da2a3c64dbd3189fef5819ad3198db6b4cc8
SumYg <61381909+SumYg@users.noreply.github.com>
2021-11-06 00:55:16 +0800

    Add files via upload

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.png';
 import './App.css';
 import React from 'react';
+import {RTC2server} from "./RTCserver_connection.js"
 
 class App extends React.Component {
   constructor(props) {
@@ -43,7 +44,10 @@ class App extends React.Component {
         break
       case 'home':
         body = <HomePage />
-      
+        break
+      default:
+        body = null
+
     }
     return (
       <React.Fragment>
@@ -60,12 +64,42 @@ function HomePage(props) {
 }
 
 function LoginPage(props) {
+  function register() {
+    document.getElementById("pw_login").style.display="none";
+    document.getElementById("face_login").style.display="none";
+    document.getElementById("register-button").style.display="none";
+    document.getElementById("welcome").innerText="Please register";
+    document.getElementById("record_face").style.display="block";
+  }
+  function record() {
+    var account = document.getElementById("account").value
+    let password = document.getElementById('password').value
+    // alert(account)
+    if (account === "" || password === "") {
+      alert("Please input both the account and password")
+      return
+    }
+    document.getElementById("record_face").style.display="none";
+    document.getElementById("video").style.display="block";
+    console.log(account)
+    RTC2server(account, true)
+  }
+  function login() {
+    alert("loging")
+    RTC2server(null, false)
+  }
+  function passwordlogin(e) {
+    e.preventDefault()
+    props.onClick()
+  }
   return <div id="login-page">
   <img style={{width:'300px', maxHeight:"50%"}} src={logo} id="logo" alt='logo'/>
   <h1 id="welcome">Welcome, Please Login</h1><div id="LoginANDRegister">
   <form id="loginForm"><p><label htmlFor="account">Account:</label><input type="text" id="account"></input></p><p><label htmlFor="password">Password:</label> <input type="password" id="password"></input></p>
-      <button id="pw_login">Login with password</button></form><button id="face_login" onClick={props.onClick}>Login with FaceID</button><button id="register-button">Register</button>
-      <button id="record_face" style={{display: "none"}} >Register your face</button>
+      <button id="pw_login" onClick={passwordlogin}>Login with password</button></form>
+      <button id="face_login" onClick={login}>Login with FaceID</button>
+      <button id="register-button" onClick={register}>Register</button>
+      <button id="record_face" style={{display: "none"}} onClick={record}>Register your face</button>
       <video width="320" height="240" id="video" autoPlay={true} playsInline={true} style={{display: "none"}}></video>
   </div>
 </div>
