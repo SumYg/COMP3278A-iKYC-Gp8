af8adc0b16a283bcc9c3f7cb2b70c4f2f7e3af7a
SumYg <61381909+SumYg@users.noreply.github.com>
2021-11-08 20:22:17 +0800

    Update sqls.py

--- sqls.py
+++ sqls.py
@@ -24,21 +24,22 @@ def getInfo():
     """
     Return username, latest login time
     """
-    query = f"SELECT username, time FROM Login_History WHERE username = '{USER_NAME}' ORDER BY time DESC LIMIT 1"
+    query = f"SELECT username, time FROM Login_History WHERE username = '{USER_NAME}' ORDER BY time DESC LIMIT 1 OFFSET 1"
     mycursor.execute(query)
     result = mycursor.fetchall()
     mydb.commit()
-    print(result)
+    print('Result: ',result)
     return result
 
 def getAllInfo():
     """
     Return username, all login time
     """
-    query = f"SELECT username, time FROM Login_History WHERE username = '{USER_NAME}' ORDER BY time DESC"
+    query = f"SELECT time FROM Login_History WHERE username = '{USER_NAME}' ORDER BY time DESC"
     mycursor.execute(query)
     result = mycursor.fetchall()
     mydb.commit()
+    result = [time[0].strftime("%Y-%m-%d %H:%M:%S") for time in result]
     print(result)
     return result
 
@@ -110,20 +111,20 @@ def createCreditAccount(username, available=0, remaining=0):
     mycursor.execute(query)
     mydb.commit()
     
-def getHKDSavingAccount():
+def getSavingAccount():
     """
-    Return account_number, currency, amount in Saving
+    Return info of user.saving account
     """
-    query = f"SELECT S.account_number, S.currency, S.amount FROM Account A, Saving S WHERE A.account_number = S.account_number AND A.username = '{USER_NAME}' AND S.currency = 'HKD'"
+    query = f"SELECT S.account_number, S.currency, S.amount FROM Account A, Saving S WHERE A.account_number = S.account_number AND A.username = '{USER_NAME}'"
     mycursor.execute(query)
     result = mycursor.fetchone()
     mydb.commit()
-    #print(result)
+    print(result)
     return result
     
 def getCreditAccount():
     """
-    Return account_number, available_credit, remaining_credit in Credit
+    Return info of user.credit account
     """
     query = f"SELECT C.account_number, C.available_credit, C.remaining_credit FROM Account A, Credit C WHERE A.account_number = C.account_number AND A.username = '{USER_NAME}'"
     mycursor.execute(query)
@@ -134,7 +135,7 @@ def getCreditAccount():
     
 def getInvestAccount():
     """
-    Return account number, amount in Investment of the user
+    Return account number, amount in table Investment 
     """
     query = f"SELECT I.account_number, I.amount FROM Account A, Investment I WHERE A.account_number = I.account_number AND A.username = '{USER_NAME}'"
     mycursor.execute(query)
@@ -145,7 +146,7 @@ def getInvestAccount():
     
 def getOwnerOfAccount(accNo):
     """
-    Return username (owner of the account)
+    Return username
     """
     query = f"SELECT username FROM Account WHERE account_number = '{accNo}'"
     mycursor.execute(query)
@@ -163,42 +164,8 @@ def checkTransAmountFromSaving(accNo, amount):
     mycursor.execute(query)
     result = mycursor.fetchone()[0]
     mydb.commit()
-    print(result >= amount)
+    #print(result >= amount)
     return (result >= amount)
-
-def countTrans():
-    query = f"SELECT COUNT(*) FROM Transaction"
-    mycursor.execute(query)
-    result = mycursor.fetchone()[0]
-    #print(result)
-    return result
-
-def updateAccount(accNo, trans_amount, fromOrTo):
-    """
-    fromOrTo == 0 -> update from_account
-    fromOrTo == 1 -> update to_account
-    """
-    if fromOrTo == 0:
-        query = f"UPDATE Saving SET amount = amount-'{trans_amount}' WHERE account_number = '{accNo}'"
-    elif fromOrTo == 1:
-        query = f"UPDATE Saving SET amount = amount+'{trans_amount}' WHERE account_number = '{accNo}'"
-    mycursor.execute(query)
-    mydb.commit()
-    print(mycursor.rowcount, "record changed.")
-
-def makeTransFromSaving(from_acc, to_acc, amount):
-    if (checkTransAmountFromSaving(from_acc, amount)):
-        trans_id = str(countTrans()+1).zfill(10)
-        current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-        current_time = datetime.datetime.now().strftime("%H:%M:%S")
-        query = f"INSERT INTO Transaction VALUES('{trans_id}', '{amount}', '{current_time}', '{current_date}', '{from_acc}', '{to_acc}')"
-        mycursor.execute(query)
-        mydb.commit()
-        print(mycursor.rowcount, "record inserted to Transaction.")
-        updateAccount(from_acc, amount, 0)
-        updateAccount(to_acc, amount, 1)
-    else:
-        return -1
     
 #register("hhh","111")
 #insertLoginHistory("John")
@@ -209,12 +176,10 @@ def makeTransFromSaving(from_acc, to_acc, amount):
 #createSavingAccount("edmund", "HKD", 0)
 #createInvestAccount("edmund", 0)
 #createCreditAccount("edmund", 10000, 10000)
-USER_NAME = "1"
+USER_NAME = "edmund"
 #getAllInfo()
-#getHKDSavingAccount()
+#getSavingAccount()
 #getCreditAccount()
 #getInvestAccount()
 #getOwnerOfAccount("00000001")
-#checkTransAmountFromSaving(getHKDSavingAccount()[0], 1000)
-#countTrans()
-#makeTransFromSaving(getHKDSavingAccount()[0], '00000001', 200)
+#checkTransAmountFromSaving(getSavingAccount()[0], 1000)
