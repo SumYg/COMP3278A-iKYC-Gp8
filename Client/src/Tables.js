2446b90f3ca481c2d8c8268ec8ba2f38f9e2b780
SumYg <61381909+SumYg@users.noreply.github.com>
2021-11-09 16:44:27 +0800

    Upload Tables.js

---
+++ Client/src/Tables.js
@@ -0,0 +1,19 @@
+export default function ArrayRecords(props) {
+    var list = [];
+    let i =0;
+    // console.log("=====",props.records)
+    let data = props.records
+    if (data != undefined) {
+        data.forEach(element => {
+            // let id = element[0]
+            // element = element[1]
+            list.push(
+                <tr id={i} key={i} className="record_row">
+                    <td><div >{element}</div></td>
+                </tr>
+            )
+            i += 1
+        });
+    }
+    return list;
+}
