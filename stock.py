89352ad401e4e92378ccedca881b92b43edc8fd1
Edmund Tsang <69718180+edmund844328@users.noreply.github.com>
2021-11-10 20:27:49 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,16 +1,28 @@
 import yfinance as yf
 import json
-
+import sqls
+import time;
 #symbol = stock symbol EX: "TSLA" or "0388.HK"
 def stock(symbol):
     data = yf.download(symbol,period='2d',time='1m')
     price_date = data[['Close']]
     price_date_dict = price_date.to_dict(orient='list')
     price_date_list = price_date_dict.get('Close')
-    previous_close = round(price_date_list[0],2)
+    #previous_close = round(price_date_list[0],2)
     current_price = round(price_date_list[1],2)
     percent_change = str(round((((price_date_list[1] - price_date_list[0]) / price_date_list[0]) *100),2)) + "%"
-    price_dict = {"Previous close": previous_close, "Current price": current_price, "Percent change": percent_change}
+    price_dict = {"stock_name": symbol, "live_price": current_price, "percentage_change": percent_change}
     json_object = json.dumps(price_dict, indent = 4)
     return json_object
 
+
+
+symbol_list = ["0005.HK","0388.HK","0700.HK","AAPL","BTC-USD","ETH-USD","FB","TSLA"]
+
+
+while (True):
+    for i in symbol_list:
+        json_data = json.loads(stock(i))
+        print(json_data["stock_name"])
+        sqls.updateStock(json_data["stock_name"],json_data["live_price"],json_data["percentage_change"])
+    time.sleep(60)
