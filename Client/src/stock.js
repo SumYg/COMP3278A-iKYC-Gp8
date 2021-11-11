f989e2638b5ec3d809384bc46f46bce468322923
SumYg <61381909+SumYg@users.noreply.github.com>
2021-11-11 12:00:15 +0800

    Update stock.js

--- Client/src/stock.js
+++ Client/src/stock.js
@@ -1,14 +1,42 @@
-  export default function StocKTable(props) {
+import React, { useState, useEffect } from 'react';
+export default function RealTimeStockTable(props) {
+    const [data, setData] = useState(props.records);
+    const update = () => {
+        fetch('http://localhost:8080/getStock').then(response => {
+            if (response.status === 200) {
+              response.json().then( res => {
+                console.log("Get")
+                console.log(res)
+                setData(res['data'])
+            })
+            } else {
+              alert("Server return status "+response.status)
+            }
+          })
+    }
 
+    useEffect(
+        () => {
+            console.log("UserEffect()")
+            const id = setInterval(update, 30000);
+            return () => clearInterval(id);
+        }
+    );
+
+    return <StocKTable records={data}/>;
+}
+
+
+function StocKTable(props) {
     var list = [];
     let i =0;
-    // console.log("=====",props.records)
+    console.log("=====",props.records)
     let data = props.records
-    if (data != undefined) {
+    if (data !== undefined) {
         data.forEach(element => {
             // let id = element[0]
             // element = element[1]
-            console.log(element)
+            console.log("StockTable update")
             list.push(
                 
                 <tr id={i} key={i} className="record_row">
