caec2aba50f8c2a1e696331a737d336ff8671400
SumYg <61381909+SumYg@users.noreply.github.com>
2021-11-11 12:00:36 +0800

    Update Tables.js

--- Client/src/Tables.js
+++ Client/src/Tables.js
@@ -9,11 +9,36 @@ export default function ArrayRecords(props) {
             // element = element[1]
             list.push(
                 <tr id={i} key={i} className="record_row">
-                    <td><div >{element}</div></td>
+                    <Array2Td records={element}/>
                 </tr>
             )
             i += 1
         });
     }
     return list;
+}
+
+const Array2Td = (props) => {
+    // console.log("=====",props.records)
+    let data = props.records
+    if (data !== undefined) {
+        if (typeof data === 'object') {
+            var list
+            list = [];
+            let i =0;
+            data.forEach(element => {
+                // let id = element[0]
+                // element = element[1]
+                list.push(
+                    <td><div id={i} key={i}>{element}</div></td>
+                )
+                i += 1
+            });
+            return list;
+        } else {
+            return <td><div >{data}</div></td>
+        }
+    }
+    
+    
 }
