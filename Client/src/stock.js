eda2fed9d73b19fe71e82f490f4d4c8c7568b3af
SumYg <61381909+SumYg@users.noreply.github.com>
2021-11-13 00:39:48 +0800

    Update stock.js

--- Client/src/stock.js
+++ Client/src/stock.js
@@ -18,7 +18,7 @@ export default function RealTimeStockTable(props) {
     useEffect(
         () => {
             console.log("UserEffect()")
-            const id = setInterval(update, 30000);
+            const id = setInterval(update, 1000);
             return () => clearInterval(id);
         }
     );
@@ -43,6 +43,8 @@ function StocKTable(props) {
                     <td><div >{element[0]}</div></td>
                     <td><div >{element[1]}</div></td>
                     <td><div >{element[2]}</div></td>
+                    <td><div >{element[3]}</div></td>
+                    <td> <Action stockName={element[0]}/></td>
                 </tr>
             )
             i += 1
@@ -50,4 +52,50 @@ function StocKTable(props) {
     }
     
     return list;
+}
+
+function Action(props) {
+    const [state, setState] = useState('show');
+    const [inputValue, setInputValue] = useState('');
+    let body;
+    function updatePosition(condition) {
+        console.log(condition, props.stockName, inputValue);
+        fetch('http://localhost:8080/updatePosition', {
+            method: "POST",
+            body: JSON.stringify([props.stockName, inputValue, condition])
+          }).then(response => {
+            if (response.status === 200) {
+              response.json().then( res => {
+                if (res) {
+                    alert('Updated Position')
+                    setState('show')
+                } else {
+                    alert('Please check your input')
+                }
+            })
+            } else {
+              alert("Server return status "+response.status)
+            }
+          })
+    }
+    switch(state) {
+        case 'show':
+            body = <div ><BuyButton setActionState={setState}/> / <SellButton setActionState={setState}/></div>
+            break
+        case 'buy':
+            body = <div><p><input onBlur={e => setInputValue(e.target.value)}></input></p><button onClick={()=>{updatePosition('Buy')}}>Submit</button><button onClick={()=>{setState('show')}}>Cancel</button></div>
+            break
+        default:
+            body = <div><p><input onBlur={e => setInputValue(e.target.value)}></input></p><button onClick={()=>{updatePosition('Sell')}}>Submit</button><button onClick={()=>{setState('show')}}>Cancel</button></div>
+
+    }
+    return body
+}
+
+function BuyButton(props) {
+    return <button onClick={() => {props.setActionState('buy')}}>Buy</button>
+}
+
+function SellButton(props) {
+    return <button onClick={() => {props.setActionState('sell')}}>Sell</button>
 }
